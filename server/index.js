a11eb9c7c94ae19c24ea189412bd52520a1eae0b
nailahgucon <nailahgucon@gmail.com>
2024-01-20 11:40:08 +0800

    test repo

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 
 db.sequelize.authenticate().then(() => {
     console.log('Connection established successfully.');
+    console.log('Connection established successfully.');
   }).catch(err => {
     console.error('Unable to connect to the database: ', err);
   });
