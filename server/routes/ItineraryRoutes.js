c18a349984a87bd533275253f1cee380774858c8
hans <hans1@outlook.sg>
2024-01-20 14:30:49 +0800

    added cruds for itinerary

--- server/routes/ItineraryRoutes.js
+++ server/routes/ItineraryRoutes.js
@@ -5,6 +5,8 @@ const auth = require("../middleware/auth_token");
 const router = require("express").Router();
 
 router.get('/all', auth.authenticateToken, itineraryController.getAllItineraries);
-// router.post('/create', auth )
+router.post('/create', auth.authenticateToken, itineraryController.createItinerary);
+router.delete('/delete', auth.authenticateToken, itineraryController.deleteItinerary);
+router.put('/edit', auth.authenticateToken, itineraryController.updateItinerary);
 
 module.exports = router;
